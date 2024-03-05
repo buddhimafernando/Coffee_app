a5743f73fe82cce3131fe1663d0c0a9ebdb241af
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-03-05 06:50:48 +0530

    googlefonts,homepage structure added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/painting.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 28, 28, 28),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right:25.0),
+            child: Icon(Icons.person),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '',
+            ),
+        ]),
+      body: Column(
+        children: [
+          //Find the best coffee for you 
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0,top: 10.0),
+            child: Text(
+              "Find the best coffee for you",
+              style: GoogleFonts.bebasNeue(
+                fontSize: 56,
+              ),
+            ),
+          ),
+          SizedBox(height: 25),
+
+          //Search bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "Find your coffee...",
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                )
+              ),
+            ),
+          ),
+          SizedBox(height: 25.0),
+          //horizontal list of coffee tiles
+          Expanded(
+            child:ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+                                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+                                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+                                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ))
+        ],
+      ),
+    );
+  }
+}
